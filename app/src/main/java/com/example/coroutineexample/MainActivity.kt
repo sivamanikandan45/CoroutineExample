3ef10e75fd7674af4407b639c195cbd5c16ea0d2
siva-pt5204 <sivamanikandan.st@zohocorp.com>
2022-07-28 19:46:30 +0530

    -

--- app/src/main/java/com/example/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutineexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.coroutineexample
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
@@ -11,12 +12,104 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        GlobalScope.launch() {
+        GlobalScope.launch {
             delay(3000L)
             setTextValue()
         }
 
-    }
+        val isactivebtn=findViewById<Button>(R.id.isactive)
+        val cancelandjoinbtn=findViewById<Button>(R.id.cancel_and_join)
+        val timeoutbtn=findViewById<Button>(R.id.timeout)
+        val timeoutOrNullbtn=findViewById<Button>(R.id.timeout_or_null)
+
+
+        isactivebtn.setOnClickListener {
+            runBlocking {
+                try{
+                    val job:Job=GlobalScope.launch {
+                        if(isActive){
+                            for(i in 1..100){
+                                //Thread.sleep(1)
+                                print("$i ")
+                            }
+                            println("End of coroutine ${Thread.currentThread().name}")
+                        }
+                    }
+                    job.cancelAndJoin()
+                }catch (ex:CancellationException){
+                    println("Cancellation exception caught")
+                }finally {
+                    withContext(NonCancellable){
+                        delay(500)
+                        println("Using suspend function inside the finally block")
+                    }
+                }
+
+                println("Currently running in ${Thread.currentThread().name}")
+            }
+
+
+        }
+
+        cancelandjoinbtn.setOnClickListener {
+
+            runBlocking {
+                try{
+                    val job:Job=GlobalScope.launch {
+                            for(i in 1..100){
+                                print("$i ")
+                                delay(100L)
+                            }
+                            println("End of coroutine ${Thread.currentThread().name}")
+                    }
+                    job.cancelAndJoin()
+                }catch (ex:CancellationException){
+                    println("Cancellation exception caught")
+                }finally {
+                    withContext(NonCancellable){
+                        delay(500)
+                        println("Using suspend function inside the finally block")
+                    }
+                }
+
+                println("Currently running in ${Thread.currentThread().name}")
+            }
+
+        }
+
+        timeoutbtn.setOnClickListener {
+            runBlocking {
+                try{
+                    withTimeout(1000){
+                        repeat(100_000){
+                            print("Hello ")
+                            delay(100)
+                        }
+                    }
+                }catch (ex:TimeoutCancellationException)
+                {
+                    println("TimeoutCancellationException caught")
+                }
+            }
+
+        }
+
+        timeoutOrNullbtn.setOnClickListener {
+            runBlocking {
+                    val ans:String?=withTimeoutOrNull(1000){
+                        repeat(100_000){
+                            print("Hello ")
+                            delay(100)
+                        }
+                        "Finished"
+                    }
+                println("$ans")
+            }
+
+        }
+        }
+
+
 
     private suspend fun setTextValue() {
         withContext(Main){
@@ -24,6 +117,5 @@ class MainActivity : AppCompatActivity() {
             textView.text="Loaded Successfully"
             textView.setTextColor(Color.BLACK)
         }
-
     }
 }
